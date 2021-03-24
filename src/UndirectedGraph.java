c68a8d1dcb6c8c698c6e35ee8c94fc0d360b807e
briantwhelan <72137130+briantwhelan@users.noreply.github.com>
2021-03-24 19:31:30 +0000

    UndirectedGraph with test class

--- src/UndirectedGraph.java
+++ src/UndirectedGraph.java
@@ -1,4 +1,157 @@
-
-public class UndirectedGraph {
-
+import java.util.HashSet;
+/*************************************************************************
+ *  Undirected Graph class.
+ *
+ *  @version 1.0 24/3/21
+ *
+ *  @author Brian Whelan
+ *
+ *************************************************************************/
+public class UndirectedGraph 
+{
+	private final int numberOfVertices;
+	private int numberOfEdges;
+	private HashSet<Integer>[] adjacencyLists;
+	
+	/**
+     * Create an empty undirected graph with a particular number of vertices
+     */
+	public UndirectedGraph(int numberOfVertices)throws IllegalArgumentException
+	{
+		if(numberOfVertices < 0)
+		{
+			throw new IllegalArgumentException("Number of vertices must be positive");
+		}
+		
+		this.numberOfVertices = numberOfVertices;
+		this.numberOfEdges = 0;
+		this.adjacencyLists = (HashSet<Integer>[]) new HashSet[numberOfVertices];
+		for(int vertex = 0; vertex < numberOfVertices; vertex++)
+		{
+			adjacencyLists[vertex] = new HashSet<Integer>();
+		}
+	}
+	
+//	/**
+//     * Create an undirected graph from an input stream
+//	 */
+//	public UndirectedGraph(In in)
+//	{
+//		this(in.readInt()); 
+//		int E = in.readInt(); 
+//		for(int i = 0; i < E; i++)
+//		{ 
+//			int v = in.readInt(); 
+//			int w = in.readInt(); 
+//			addEdge(v, w);
+//		}
+//	}
+	
+	/**
+     * Add an edge to the undirected graph
+     * 
+     * @param vertex1: first vertex of edge to be added
+     * @param vertex2: second vertex of edge to be added
+     */
+    public void addEdge(int vertex1, int vertex2)
+    {
+    	if((vertex1 >= 0) && (vertex1 < numberOfVertices)
+    		&& (vertex2 >= 0) && (vertex2 < numberOfVertices))
+    	{
+	    	adjacencyLists[vertex1].add(vertex2);
+	    	adjacencyLists[vertex2].add(vertex1);
+	    	numberOfEdges++;
+    	}
+    }
+    
+    /**
+     * Get adjacency list for a particular vertex
+     * 
+     * @param vertex: vertex of which adjacency list is required
+     * @return vertices adjacent to vertex
+     */
+    public Iterable<Integer> getAdjacencyList(int vertex)
+    {
+    	Iterable<Integer> adjacencyList = null;
+    	if((vertex >= 0) && (vertex < numberOfVertices))
+    	{
+    		adjacencyList = adjacencyLists[vertex];
+    	}
+    	
+    	return adjacencyList;
+    }
+    
+    /**
+     * Get degree of a particular vertex
+     * 
+     * @param vertex: vertex of which degree is required
+     * @return degree of vertex
+     */
+    public int getDegree(int vertex)
+    {
+    	int degree = -1;
+    	if((vertex >= 0) && (vertex < numberOfVertices))
+    	{
+	    	degree = 0;
+	    	for(int adjacentVertex : getAdjacencyList(vertex))
+	    	{
+	    		degree++;
+	    	}
+    	}
+    	
+    	return degree;
+    }
+    
+    /**
+     * Get number of vertices in undirected graph
+     * 
+     * @return number of vertices in undirected graph
+     */
+    public int getNumberOfVertices()
+    {
+    	return numberOfVertices;
+    }
+    
+    /**
+     * Get number of edges in undirected graph
+     * 
+     * @return number of edges in undirected graph
+     */
+    public int getNumberOfEdges()
+    {
+    	//return numberOfEdges;
+    	numberOfEdges = 0;
+    	for(int vertex = 0; vertex < numberOfVertices; vertex++)
+	    {
+		    for(int adjacentVertex : getAdjacencyList(vertex))
+		    {
+		    	numberOfEdges++;
+		    }
+	    }
+    	numberOfEdges /= 2;
+    	
+    	return numberOfEdges;
+    }
+    
+    /**
+     * Get String representation of graph's vertices and edges
+     * 
+     * @return String containing information on graph's vertices and edges
+     */
+    @Override
+    public String toString()
+    {
+	    String string =  getNumberOfVertices() + " vertices, " + getNumberOfEdges() + " edges\n";
+	    for(int vertex = 0; vertex < numberOfVertices; vertex++)
+	    {
+		    string += vertex + ": ";
+		    for(int adjacentVertex : getAdjacencyList(vertex))
+		    {
+		    	string += adjacentVertex + " ";
+		    }
+		    string += "\n";
+	    }
+	    
+	    return string;
+    }
 }
--- src/UndirectedGraphTest.java
+++ src/UndirectedGraphTest.java
@@ -1,4 +1,147 @@
+import static org.junit.Assert.assertEquals;
 
-public class UndirectedGraphTest {
+import java.util.Arrays;
+import java.util.HashSet;
 
+import org.junit.Test;
+/*************************************************************************
+ *  Undirected Graph test class.
+ *
+ *  @version 1.0 24/3/21
+ *
+ *  @author Brian Whelan
+ *
+ *************************************************************************/
+public class UndirectedGraphTest 
+{
+	/**
+     * Test constructor
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructor()
+    {
+    	//Test trying to create invalid graph
+    	new UndirectedGraph(-1);
+    	
+    	//Test trying to create valid graph
+    	new UndirectedGraph(10);
+    }
+    
+	/**
+     * Test addEdge()
+     */
+    @Test
+    public void testAddEdge()
+    {
+    	UndirectedGraph graph = new UndirectedGraph(3);
+    	
+    	//Test adding invalid edges
+    	graph.addEdge(1, 5);
+    	assertEquals("Testing addEdge for an invalid edge", 0, graph.getNumberOfEdges());
+    	graph.addEdge(4, 1);
+    	assertEquals("Testing addEdge for an invalid edge", 0, graph.getNumberOfEdges());
+    	graph.addEdge(1, -2);
+    	assertEquals("Testing addEdge for an invalid edge", 0, graph.getNumberOfEdges());
+    	graph.addEdge(-1, 1);
+    	assertEquals("Testing addEdge for an invalid edge", 0, graph.getNumberOfEdges());
+    	
+    	//Test adding a valid edge
+    	graph.addEdge(1, 2);
+    	assertEquals("Testing addEdge for a valid edge", 1, graph.getNumberOfEdges());
+    	
+    	//Test adding a duplicate edge
+    	graph.addEdge(1, 2);
+    	assertEquals("Testing addEdge for a duplicate edge", 1, graph.getNumberOfEdges());   	
+    }
+    
+    /**
+     * Test getAdjacencyList()
+     */
+    @Test
+    public void testGetAdjacencyList()
+    {
+    	UndirectedGraph graph = new UndirectedGraph(3);
+    	
+    	//Test getting an invalid adjacency list for invalid vertices
+    	assertEquals("Testing getAdjacencyList for an invalid vertex", null, graph.getAdjacencyList(4));
+    	assertEquals("Testing getAdjacencyList for an invalid vertex", null, graph.getAdjacencyList(-1));
+    	
+    	//Test getting an empty adjacency list for a valid vertex
+    	assertEquals("Testing getAdjacencyList for a valid vertex", new HashSet<Integer>(), graph.getAdjacencyList(0));
+    	
+    	//Test getting a non-empty adjacency list for a valid vertex
+    	graph.addEdge(0, 1);
+    	graph.addEdge(0, 2);
+    	assertEquals("Testing getAdjacencyList for a valid vertex", new HashSet<Integer>(Arrays.asList(1, 2)), graph.getAdjacencyList(0));    
+    }
+    
+    /**
+     * Test getDegree()
+     */
+    @Test
+    public void testGetDegree()
+    {
+    	UndirectedGraph graph = new UndirectedGraph(3);
+    	
+    	//Test getting degree of invalid vertices
+    	assertEquals("Testing getDegree for an invalid vertex", -1, graph.getDegree(4));
+    	assertEquals("Testing getDegree for an invalid vertex", -1, graph.getDegree(-1));
+    	
+    	//Test getting degree of a valid vertex with degree 0
+    	assertEquals("Testing getDegree for an valid vertex", 0, graph.getDegree(0));
+    	
+    	//Test getting degree of a valid vertex with degree 2
+    	graph.addEdge(0, 1);
+    	graph.addEdge(0, 2);
+    	assertEquals("Testing getDegree for an valid vertex", 2, graph.getDegree(0));  
+    }
+    
+    /**
+     * Test getNumberOfVertices()
+     */
+    @Test
+    public void testGetNumberOfVertices()
+    {    	
+    	//Test getting number of vertices in a graph which has 0 vertices
+    	UndirectedGraph graph = new UndirectedGraph(0);
+    	assertEquals("Testing getNumberOfVertices", 0, graph.getNumberOfVertices());
+    	
+    	//Test getting number of vertices in a graph which has 3 vertices
+    	graph = new UndirectedGraph(3);
+    	assertEquals("Testing getNumberOfVertices", 3, graph.getNumberOfVertices());  
+    }
+    
+    /**
+     * Test getNumberOfEdges()
+     */
+    @Test
+    public void testGetNumberOfEdges()
+    {    	
+    	UndirectedGraph graph = new UndirectedGraph(3);
+    	
+    	//Test getting number of edges in a graph which has 0 edges
+    	assertEquals("Testing getNumberOfEdges", 0, graph.getNumberOfEdges());
+    	
+    	//Test getting number of vertices in a graph which has 2 edges
+    	graph.addEdge(0, 1);
+    	graph.addEdge(0, 2);
+    	assertEquals("Testing getNumberOfEdges", 2, graph.getNumberOfEdges());  
+    }
+    
+    /**
+     * Test toString()
+     */
+    @Test
+    public void testToString()
+    {    	
+    	//Test toString on empty graph
+    	UndirectedGraph graph = new UndirectedGraph(0);
+    	assertEquals("Testing toString on empty graph", "0 vertices, 0 edges\n", graph.toString());
+    	
+    	//Test getting number of vertices in a graph which has 2 edges
+    	graph = new UndirectedGraph(3);
+    	graph.addEdge(0, 1);
+    	graph.addEdge(0, 2);
+    	assertEquals("Testing toString on non-empty graph", "3 vertices, 2 edges\n0: 1 2 \n1: 0 \n2: 0 \n", graph.toString());  
+    }
 }
